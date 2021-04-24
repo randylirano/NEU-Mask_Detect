02cf24fb39088982ffc398d7686ea657c6a0378e
Steve <zjysteve@gmail.com>
2021-04-24 15:45:21 -0700

    Add button for photo uploading

--- face-mask-ui/src/app/product/product.component.ts
+++ face-mask-ui/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  fileData: File = null;
+  previewUrl:any = null;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onFileSelected(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+    this.preview();
+  }
+
+  preview() {
+    // Show preview
+    const mimeType = this.fileData.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileData);
+    reader.onload = (_event) => {
+      this.previewUrl = reader.result;
+    }
+  }
+
+  onFileUploaded() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post('', formData)
+      .subscribe(res => {
+        console.log(res);
+        alert('SUCCESS !!');
+      })
+  }
 }
