fd6c3e50fe9286dcaae78621372e97b516b428a7
Shruthi Raghuraman <shruthiraghuraman@MacBook-Pro.home>
2021-04-26 02:57:54 -0400

    Our team page design

--- face-mask-ui/src/app/story/story.component.ts
+++ face-mask-ui/src/app/story/story.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Inject  } from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-story',
   templateUrl: './story.component.html',
-  styleUrls: ['./story.component.css']
+  styleUrls: ['./story.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class StoryComponent implements OnInit {
+export class StoryComponent implements OnInit, OnDestroy  {
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private _document ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._document.body.classList.add('teambody-color');
   }
+
+  ngOnDestroy() {
+  // remove the class form body tag
+  this._document.body.classList.remove('teambody-color');
+}
 
 }
